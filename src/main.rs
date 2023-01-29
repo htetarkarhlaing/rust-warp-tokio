ebcb96ec5dc1e1e51e30d9c21027e2e97ac9104a
Htet Arkar Hlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-01-29 15:10:46 +0630

    with params

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use warp::Filter;
 
-async fn hello_handler() -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(format!("Hello World."))
+async fn hello_handler(param: String) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Hello World. {}", param))
 }
 
 #[tokio::main]
 async fn main() {
     let hello = warp::get()
         .and(warp::path("hello"))
+        .and(warp::path::param::<String>())
         .and(warp::path::end())
         .and_then(hello_handler);
 
